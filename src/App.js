3fee34b009658093d9f2ebb00f7c5c5ae2086bff
Nabham <nnssuser@gmail.com>
2021-09-08 22:48:10 +0530

    new merged cart feature

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import CartProvider from './store/CartProvider';
-<<<<<<< HEAD
 import Meals from './components/Meals/Meals';
-=======
 import Cart from './components/Cart/Cart';
->>>>>>> feat/cart
-
 function App() {
 
   const [cartIsShown, setCartIsShown] = useState(false);
-
 
   function showCartHandler() {
     setCartIsShown(true);
